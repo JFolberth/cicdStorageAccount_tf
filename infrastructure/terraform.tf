ba022cb64e7d83cd4015986cc1131a664e40dd31
John Folberth <johnfolberth@gmail.com>
2024-11-22 14:00:29 -0500

    Configure Azure backend with resource group, storage account, and container for Terraform state management

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 1.5.2"
-  backend  "azurerm"{}
+  backend  "azurerm"{
+    resource_group_name  = "my-tf-remote-state-rg-name"
+    storage_account_name = "my-tf-remote-state-sa-name"
+    container_name       = "my-tf-remote-state-container-name"
+    key                  = "my-state-file-name.tfstate"
+  }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
