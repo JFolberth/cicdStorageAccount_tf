8b92e03eabc16c7fd71613d7091bbf3e10a862d2
John Folberth <johnfolberth@gmail.com>
2024-11-25 11:08:45 -0500

    Enable Azure AD authentication and OIDC in Terraform configuration; update provider version constraint

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -5,11 +5,13 @@ terraform {
     storage_account_name = "my-tf-remote-state-sa-name"
     container_name       = "my-tf-remote-state-container-name"
     key                  = "my-state-file-name.tfstate"
+
+
   }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71"
+      version = "~S> 3.71"
     }
     random = {
       source  = "hashicorp/random"
