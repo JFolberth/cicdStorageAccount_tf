2dd2d3d4d518412dff474e81b9ebbfc759a6c05e
John Folberth <johnfolberth@gmail.com>
2024-11-25 11:09:26 -0500

    Fix version constraint syntax for azurerm provider in Terraform configuration

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~S> 3.71"
+      version = "~> 3.71"
     }
     random = {
       source  = "hashicorp/random"
